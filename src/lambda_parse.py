e5b538bfb7b9801ac3b31170050d818a41ae2693
AgentElement <agentelement@protonmail.com>
2023-09-12 12:57:43 -0700

    feat: edge and vertex iterators

--- src/lambda_parse.py
+++ src/lambda_parse.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 from enum import Enum
 import re
 
+import collections
+
 
 class ASTNode:
     def __init__(self, left: ASTNode, right: ASTNode):
         self.left: ASTNode = left
         self.right: ASTNode = right
         self.value: Token.VAR = None
+        self.id: int = 0
         self.depth: int = 0
 
     def set_value(self, value: Token.VAR) -> ASTNode:
@@ -16,6 +19,10 @@ class ASTNode:
 
     def set_depth(self, depth: int) -> ASTNode:
         self.depth = depth
+        return self
+
+    def set_id(self, id: int) -> ASTNode:
+        self.id = id
         return self
 
     def __str__(self) -> str:
@@ -30,12 +37,12 @@ class ASTNode:
         for line in lines:
             print(line)
 
-    def _display_aux(self):
+    def _display_aux(self, ob=lambda x: x.id):
         """Returns list of strings, width, height, and horizontal coordinate
         of the root."""
         # No child.
         if self.right is None and self.left is None:
-            line = f"{self.value}"
+            line = f"{ob(self)}"
             width = len(line)
             height = 1
             middle = width // 2
@@ -44,7 +51,7 @@ class ASTNode:
         # Only left child.
         if self.right is None:
             lines, n, p, x = self.left._display_aux()
-            s = f"{self.value}"
+            s = f"{ob(self)}"
             u = len(s)
             first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
             second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
@@ -54,7 +61,7 @@ class ASTNode:
         # Only right child.
         if self.left is None:
             lines, n, p, x = self.right._display_aux()
-            s = f"{self.value}"
+            s = f"{ob(self)}"
             u = len(s)
             first_line = s + x * '_' + (n - x) * ' '
             second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
@@ -64,7 +71,7 @@ class ASTNode:
         # Two children.
         left, n, p, x = self.left._display_aux()
         right, m, q, y = self.right._display_aux()
-        s = f"{self.value}"
+        s = f"{ob(self)}"
         u = len(s)
         first_line = (x + 1) * ' ' + (n - x - 1) * \
             '_' + s + y * '_' + (m - y) * ' '
@@ -78,6 +85,24 @@ class ASTNode:
         lines = [first_line, second_line] + \
             [a + u * ' ' + b for a, b in zipped_lines]
         return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+    def edges_breadth(self):
+        queue = collections.deque([self])
+        while queue:
+            parent = queue.popleft()
+            for child in (parent.left, parent.right):
+                if child:
+                    yield ((parent.id, child.id))
+                    queue.append(child)
+
+    def vertices_breadth(self):
+        queue = collections.deque([self])
+        while queue:
+            parent = queue.popleft()
+            yield ((parent.id, parent.value))
+            for child in (parent.left, parent.right):
+                if child:
+                    queue.append(child)
 
 
 class TokenType(Enum):
@@ -157,10 +182,13 @@ class LambdaParser:
     # term := lambda | lambda term
     # lambda := abs | ( term ) | id
     # main := lambda EOF
+        self.index = 0
 
     def parse(self) -> ASTNode:
+        self.index = 0
         expr = self.parse_term()
         self.lexer.eat(TokenType.EOF)
+        self.index = 0
         return expr
 
     def parse_abstraction(self) -> ASTNode:
@@ -169,7 +197,9 @@ class LambdaParser:
         self.lexer.eat(TokenType.DOT)
         ltree = self.parse_term()
 
-        node = ASTNode(ltree, None).set_value(lvar)
+        self.index += 1
+
+        node = ASTNode(ltree, None).set_value(lvar).set_id(self.index)
         return node
 
     def parse_term(self) -> ASTNode:
@@ -177,7 +207,8 @@ class LambdaParser:
         rtree = None
         if self.lexer.peek(1).tok_type in {TokenType.LAMBDA, TokenType.LBRACE, TokenType.VAR}:
             rtree = self.parse_term()
-            return ASTNode(ltree, rtree)
+            self.index += 1
+            return ASTNode(ltree, rtree).set_id(self.index)
         else:
             return ltree
 
@@ -192,7 +223,8 @@ class LambdaParser:
                 return term
             case TokenType.VAR:
                 lvar = self.lexer.eat(TokenType.VAR)
-                node = ASTNode(None, None).set_value(lvar)
+                self.index += 1
+                node = ASTNode(None, None).set_value(lvar).set_id(self.index)
                 return node
             case _:
                 # "snytax rrrrrr" is a reference to Prof. Rida Bazzi
@@ -204,6 +236,9 @@ def main():
     parser = LambdaParser(lexer)
     ast = parser.parse()
     ast.display()
+
+    print([e for e in ast.edges_breadth()])
+    print([v for v in ast.vertices_breadth()])
 
 
 if __name__ == '__main__':
